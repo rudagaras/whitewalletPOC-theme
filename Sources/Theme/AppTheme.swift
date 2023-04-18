020a044c2e44b6386e7839fb80d9d787e215bc9e
Ruben Garcia <ruben.garcia@globant.com>
2023-04-18 07:46:41 -0500

    Initial commit

---
+++ Sources/Theme/AppTheme.swift
@@ -0,0 +1,12 @@
+//
+//  AppTheme.swift
+//  
+//
+//  Created by Ruben Dario Garcia Astudillo on 18/04/23.
+//
+
+import Foundation
+
+public struct AppTheme {
+    public static let color: AppColor = AppColor()
+}
---
+++ Sources/Theme/Color/AppColor.swift
@@ -0,0 +1,14 @@
+//
+//  AppColor.swift
+//  
+//
+//  Created by Ruben Dario Garcia Astudillo on 18/04/23.
+//
+
+import SwiftUI
+
+public struct AppColor {
+    public let primary: LocalizedStringKey = "primary-color"
+    public let secondary: LocalizedStringKey = "secondary-color"
+    public let tertiary: LocalizedStringKey = "tertiary-color"
+}
